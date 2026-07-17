90422359dd84ea3821f9e55436a6e3556f70248d
Pavankumar Madavi <pavank.madavi@gmail.com>
2026-07-18 05:14:57 +0530

    Add app theme and Android resource files

---
+++ app/src/main/java/com/nidar/mbc/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.nidar.mbc.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF1565C0)
+val PrimaryDark = Color(0xFF003C8F)
+val Secondary = Color(0xFF26A69A)
+val Background = Color(0xFFF5F5F5)
+val Surface = Color(0xFFFFFFFF)
+val Error = Color(0xFFD32F2F)
+
+val OnPrimary = Color.White
+val OnSecondary = Color.White
+val OnBackground = Color.Black
+val OnSurface = Color.Black
